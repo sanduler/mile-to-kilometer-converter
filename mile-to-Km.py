01bbef14c05e180baba04b6739a0d71bc685510a
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-08 22:26:17 -0800

    changed the name and created the window

---
+++ mile-to-Km.py
@@ -0,0 +1,13 @@
+# Name: Ruben Sanduleac
+# Description: A simple GUI using tkinter for converting mile to kilometer
+from tkinter import *
+
+# create a window object from the Tk class
+window = Tk()
+# change the title of the GUI
+window.title("Mile to Km Converter")
+# set the size of the window
+window.minsize(width=300, height=100)
+
+# prevent window from closing
+window.mainloop()
